cf7d1008e5fdc700935a540af19c851fcd9e94ae
SaadVSP96 <saadmasood192@gmail.com>
2024-11-24 20:47:09 +0500

    6. 20% - Route Params Part 3.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 function App() {
     return (
@@ -19,6 +20,9 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/vans" element={<Vans />} />
+                {/* the colon makes the next word act as a key for the
+                 KVP it will generate using the value in the link */}
+                <Route path="/vans/:id" element={<VanDetail />} />
             </Routes>
         </BrowserRouter>
     );
