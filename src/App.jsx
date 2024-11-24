91aa43d8467dd7ef59f01c5a34cbdcb022f3bdc9
SaadVSP96 <saadmasood192@gmail.com>
2024-11-24 17:41:53 +0500

    2. 9% course completion - Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function About() {
+    return <h1>ABOUT Page</h1>;
+}
+
+function Home() {
+    return <h1>HOME Page</h1>;
+}
 
 function App() {
-    return <h1>HELLOOO</h1>;
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
