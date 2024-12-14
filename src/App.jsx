d0a697e76adc2db2d915eea19b6b1ccde02ee20a
SaadVSP96 <saadmasood192@gmail.com>
2024-12-14 19:39:03 +0500

    9. Freezing project before adding authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About from "./pages/About";
 import Home from "./pages/Home";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
+import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetails";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
-import Dashboard from "./Host/Dashboard";
-import Reviews from "./Host/Reviews";
-import Income from "./Host/Income";
 import HostLayout from "./components/HostLayout";
-import HostVans from "./Host/HostVans";
-import HostVanDetails from "./Host/HostVanDetails";
 
-function App() {
+import "./server";
+
+export default function App() {
     return (
         <BrowserRouter>
             <Routes>
@@ -26,12 +34,18 @@ function App() {
                         <Route path="income" element={<Income />} />
                         <Route path="reviews" element={<Reviews />} />
                         <Route path="vans" element={<HostVans />} />
-                        <Route path="vans/:id" element={<HostVanDetails />} />
+                        <Route path="vans/:id" element={<HostVanDetail />}>
+                            <Route index element={<HostVanInfo />} />
+                            <Route
+                                path="pricing"
+                                element={<HostVanPricing />}
+                            />
+                            <Route path="photos" element={<HostVanPhotos />} />
+                        </Route>
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
         </BrowserRouter>
     );
 }
-
-export default App;
