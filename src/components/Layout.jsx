e5079bde3e7a1d59a4024f523ecf3e0b328cf0b0
fahad-169 <fahadnvidia@gmail.com>
2025-04-26 14:51:11 +0500

    chatbot now works

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout() {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "//code.tidio.co/bqs9p8vtubrs3swhah5994rxduhllvvg.js";
+        script.async = true;
+        document.body.appendChild(script);
+      }, []);
+
     return (
         <div className="site-wrapper">
             <Header />
             <main>
                 <Outlet />
+            <script src="//code.tidio.co/bqs9p8vtubrs3swhah5994rxduhllvvg.js" async></script>
             </main>
             <Footer />
         </div>
